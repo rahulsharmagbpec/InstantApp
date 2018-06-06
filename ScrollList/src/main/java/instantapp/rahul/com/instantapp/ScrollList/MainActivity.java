70eeabc64b0ef21c6ff4ba93f51645c63ecb96a6
rahulsharma <aozps8819>
2018-06-06 23:24:35 +0530

    instant app sample not working in miui

---
+++ ScrollList/src/main/java/instantapp/rahul/com/instantapp/ScrollList/MainActivity.java
@@ -0,0 +1,18 @@
+package instantapp.rahul.com.instantapp.ScrollList;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Intent appLinkIntent = getIntent();
+        String appLinkAction = appLinkIntent.getAction();
+        Uri appLinkData = appLinkIntent.getData();
+    }
+}
